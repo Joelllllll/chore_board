19c2fb6cabbb209c9267684378992406e74ab210
Joel Hendy <joel.hendy@lexer.com.au>
2020-04-13 19:11:07 +1000

    Added db reset/init

--- forms.py
+++ forms.py
@@ -1,13 +1,18 @@
 from wtforms import StringField, SubmitField, IntegerField, validators
 from flask_wtf import FlaskForm
 
-## Forms
+## New Chore
 class NewChoreForm(FlaskForm):
     username = StringField('User', [validators.Length(min=3, max=25), validators.DataRequired()])
     chore = StringField('Chore', [validators.DataRequired()])
     submit = SubmitField('Submit')
 
+## Delete Chore
 class DeleteChoreForm(FlaskForm):
     username = StringField('User', [validators.Length(min=3, max=25), validators.DataRequired()])
     id = IntegerField('Chore ID', [validators.DataRequired()])
     submit = SubmitField('Submit')
+
+## Reset Database
+class ResetDatabaseConfirm(FlaskForm):
+    submit = SubmitField('OK')
--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Chores(db.Model):
 ##----------------------------------------ROUTES----------------------------------------##
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('home.html')
 
 ## New Chore
 @app.route('/new_chore', methods=['GET', 'POST'])
@@ -35,33 +35,48 @@ def new_chore():
     if form.validate_on_submit():
         db.session.add(Chores(user=form.username.data, chore=form.chore.data))
         db.session.commit()
-        redirect('/new_chore')
         ## Empty the fields
         form = forms.NewChoreForm(formdata=None)
     return render_template('new_chores.html', form=form)
-
-## Chore Table
-@app.route('/chore_board', methods=['GET', 'POST'])
-def display_chore_board():
-    ## sqlite doesn't do disticnt on() so we need to work around it a little
-    chores = db.engine.execute(
-        """ SELECT id, user, chore, MAX(datetime(datetime)) AS datetime, ((STRFTIME('%s','NOW') + 10 * 3600) - STRFTIME('%s',"datetime")) / 3600 AS diff
-        FROM chores
-        GROUP BY chore"""
-        )
-    return render_template('chore_board.html', table=chores, data=chores)
 
 ## Delete Chore
 @app.route('/delete_chore', methods=['GET', 'POST'])
 def delete_chore():
     form = forms.DeleteChoreForm()
     if form.validate_on_submit():
-        x = Chores.query.filter_by(id=form.id.data).delete()
+        Chores.query.filter_by(id=form.id.data).delete()
         db.session.commit()
-        redirect('/delete_chore')
         ## Empty the fields
         form = forms.DeleteChoreForm(formdata=None)
     return render_template('delete_chore.html', form=form)
+
+## Chore Table
+@app.route('/chore_board', methods=['GET', 'POST'])
+def display_chore_board():
+    ## sqlite doesn't do disticnt on() so we need to work around it a little
+    chores = db.engine.execute(
+        """SELECT id, user, chore, MAX(datetime(datetime)) AS datetime, ((STRFTIME('%s','NOW') + 10 * 3600) - STRFTIME('%s',"datetime")) / 3600 AS diff
+        FROM chores
+        GROUP BY chore
+        ORDER BY datetime DESC"""
+        )
+    return render_template('chore_board.html', table=chores, data=chores)
+
+## DB init
+@app.route('/db_init')
+def initiate_database():
+    db.create_all()
+    return render_template('chore_board.html')
+
+## DB Delete
+@app.route('/db_delete', methods = ['GET', 'POST'])
+def delete_database():
+    form = forms.ResetDatabaseConfirm()
+    if form.validate_on_submit():
+        db.drop_all()
+        db.create_all()
+        db.session.commit()
+    return render_template('reset_database.html', form=form)
 
 ##----------------------------------------MAIN----------------------------------------##
 
@@ -69,7 +84,6 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', threaded=True)
 
 ## TODO:
-# Have a reset DB button
 # Actual log-in feature
 # change column hours ago to be days when > 24 hours
 # Make requirements file
